a6a098609ece90baab2060c0a657b3635f9886d7
alexws23 <alexws23@verizon.net>
2026-01-27 13:34:14 -0600

    data prep

--- motus_filtering.r
+++ motus_filtering.r
@@ -3,7 +3,9 @@
 library(tidyverse)
 
 #Load in dataset
-data <- read.csv("C:/Users/awsmilor/Box/Kennekuk Sigma 8 Data/fall_individual_birds/261 .csv")
+data <- read.csv("~/Library/CloudStorage/Box-Box/Kennekuk Sigma 8 Data/fall_individual_birds/261 .csv"
+  #"C:/Users/awsmilor/Box/Kennekuk Sigma 8 Data/fall_individual_birds/261 .csv"
+  )
 
 #Definitions
 #Run = A collections of consecutive hits. Up to 60 bursts can be missed (not detected) before a new run starts.
@@ -13,4 +15,13 @@ run_gap <- 60*7.098899841
 
 # Arrange by Date
 data <- data %>% 
-  arrange(Datetime)
+  group_by(port) %>% 
+  arrange(Datetime) %>% 
+  mutate(run_id = 1:n(),
+         gap_start = as_datetime(lag(Datetime)),
+         gap_end = as_datetime(Datetime),
+         Datetime = as_datetime(Datetime),
+         diff = difftime(gap_end, gap_start),
+         run_id = ifelse(diff < run_gap, lag(run_id), run_id)
+         )
+ 
